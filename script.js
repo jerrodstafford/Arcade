d288a5028d894ab5158b25cd56e543df27bb850d
Jerrod Stafford <jerrodstafford10@gmail.com>
2022-08-20 04:41:47 -0500

    Friday Night Updates pt 1: created functions for player name and player turn, as well as creating better functionality within cells

--- script.js
+++ script.js
@@ -1,30 +1,65 @@
-// State of board (changes)
+// INITIAL STATE
 const state = {};
 
 
 const resetState = () => {
-    state.board = ['','','','','','','','',''];
+    state.board = [ 
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+
+    state.players = ['', ''];
+    state.currentPlayerIndx = 0;
 }
+
+//HELPER FUNCTIONS - Small functions that do basic things
+const changeTurn = () => {
+    state.currentPlayerIndx = state.currentPlayerIndx === 0 ? 1 : 0;
+} 
+
+const getCurrentPlayer = () => state.players[state.currentPlayerIndx];
+
+const render = () => {
+    renderPlayer();
+    renderBoard();
+}
+
+
 
 // DOM Selectors
 const boardElem = document.getElementById('board');
+const playerTurnElem = document.getElementById('player-turn');
 
 // DOM Manipulators
 const renderBoard = () => {
     boardElem.innerHTML = ''
+    let flattenedBoard = state.board.flat(state.board.length);
 
-    for(let i = 0; i < state.board.length; i++) {
-        const input = state.board[i];
+    for(let i = 0; i < flattenedBoard.length; i++) { 
+        const square = flattenedBoard[i];
         const cellElem = document.createElement('div');
         cellElem.classList.add('cell');
-        const symbolElem = document.createElement('span');
-        symbolElem.classList.add('symbol');
-        symbolElem.innerHTML = input;
+        cellElem.innerHTML = square;
         cellElem.dataset.index = i;
-        cellElem.appendChild(symbolElem);
         boardElem.appendChild(cellElem);
-    }
     
+  }    
+}
+
+const renderPlayer = () => {
+    let text;
+    if(!state.players[0] || !state.players[1]) {
+        text = `
+        <input name="player1" placeholder="Enter player 1's name">
+        <input name="player2" placeholder="Enter player 2's name">
+        <button class="start">Start</button>
+        `
+    } else {   
+        text = `It's currently ${getCurrentPlayer()}'s turn`
+    }
+        playerTurnElem.innerHTML = text;
+
 }
 
 // EVENT LISTENERS
@@ -32,14 +67,31 @@ boardElem.addEventListener('click', (event) => {
     if(event.target.className !== 'cell') return;
 
     let cellIndex = event.target.dataset.index
-    state.board[cellIndex] = '';
+    // state.board[cellIndex] = ;
 
-    renderBoard();
+    changeTurn();
+    render();
 })
+
+playerTurnElem.addEventListener('click', (event) => {
+    if(event.target.className !== 'start') return;
+ 
+    // console.log('I am Her');
+    const player1Input = document.getElementsByName('player1')[0];
+    state.players[0] = player1Input.value;
+
+    const player2Input = document.getElementsByName('player2')[0];
+    state.players[1] = player2Input.value;
+    
+    console.log(state.players);
+    render();
+}) 
+
+
 
 // BOOSTRAP FUNCTIONS
 resetState();
-renderBoard();
+render();
 
 
 
